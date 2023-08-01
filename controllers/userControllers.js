2c78d2c8c565a07064a42f66e1465ad89779c351
park <park>
2023-08-01 20:18:28 +0900

    유저 정보 업데이트 코드 모듈화

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -5,26 +5,39 @@ const jwt = require('jsonwebtoken')
 const bcrypt = require('bcrypt')
 require('dotenv').config()
 
-const updateProps = async (req, res, props, propName) => {
+const getUserFromToken = async (req) => {
     const token = req.headers.authorization.split(' ')[1];
-    try {
-        const decodedToken = jwt.verify(token, process.env.REACT_APP_JWT_SECRET);
-        const userId = decodedToken._id;
-        const user = await User.findById(userId);
-        if (!user) {
-            return res.status(404).json({ message: 'User not found' });
-        }
-        const updateData = { $set: { [propName]: props[propName] } };
-        const updatedUser = await User.updateOne({ _id: userId }, updateData);
+    const decodedToken = jwt.verify(token, process.env.REACT_APP_JWT_SECRET);
+    const userId = decodedToken._id;
+    const user = await User.findById(userId);
+    return user;
+};
+const checkUserExists = (user, res) => {
+    if (!user) {
+        return res.status(404).json({ message: 'User not found' });
+    }
+};
 
+const updateProps = async (req, res, props, propName) => {
+    try {
+        const user=await getUserFromToken(req)
+        checkUserExists(user, res);
+        const updateData = { $set: { [propName]: props[propName] } };
+        const updatedUser = await User.updateOne({ _id: user._id }, updateData);
         if (updatedUser.n === 0) {
             return res.status(404).json({ message: 'User not found' });
         }
-        // return res.json({ state: true });
         return true
     } catch (error) {
         console.error(error);
         res.status(500).json({ message: 'Internal Server Error' });
+    }
+};
+const updateProfile = async (req, res, propName) => {
+    const dataToUpdate = req.body;
+    const updated = await updateProps(req, res, dataToUpdate, propName);
+    if (updated) {
+        return res.json({ state: true });
     }
 };
 
@@ -32,12 +45,11 @@ let verifyCodes = {}
 const userControl = {
     sendVerificationCode: async (req, res) => {
         try {
-            const email = req.body.email;
+            const {email} = req.body
             const verifyCode = Array.from({length: 6}, () =>
                 Math.floor(Math.random() * 10)
             ).join("");
             verifyCodes[email] = verifyCode;
-
             const transporter = nodemailer.createTransport({
                 service: "gmail",
                 auth: {
@@ -45,16 +57,13 @@ const userControl = {
                     pass: process.env.REACT_APP_NODEMAILER_PASS,
                 },
             });
-
             const mailOptions = {
                 from: process.env.REACT_APP_NODEMAILER_USER,
                 to: email,
                 subject: "Verification Code",
                 text: `Your verification code is: ${verifyCode}`,
             };
-
             await transporter.sendMail(mailOptions);
-
             res.status(200).send(`인증번호가 이메일로 전송되었습니다. ${verifyCode}`);
         } catch (error) {
             res.status(400).send("Invalid email address");
@@ -62,7 +71,6 @@ const userControl = {
     },
     verifyCode: (req, res) => {
         const {email, verificationCode} = req.body;
-
         if (verifyCodes[email] && verifyCodes[email] === verificationCode) {
             res.status(200).send("Verification successful");
         } else {
@@ -114,22 +122,9 @@ const userControl = {
             return res.status(500).json({message: `Internal Server Error`})
         }
     },
-    getUserBasicInfo: (req, res) => {
-        const token = req.headers.authorization.split(" ")[1];
-        jwt.verify(token, process.env.REACT_APP_JWT_SECRET, (err, decoded) => {
-            if (err) return res.status(401).json({message: err})
-            const user = {
-                name: decoded.name,
-                email: decoded.email
-            }
-            res.json(user)
-        })
-    },
     getUserFullInfo: async (req, res) => {
         try {
-            const token = req.headers.authorization.split(" ")[1];
-            const decoded = jwt.verify(token, process.env.REACT_APP_JWT_SECRET);
-            const user = await User.findOne({email: decoded.email});
+            const user=await getUserFromToken(req)
             res.json(user);
         } catch (err) {
             console.error(err);
@@ -147,14 +142,9 @@ const userControl = {
     },
     followUser: async (req, res) => {
         const {userIdToFollow} = req.body
-        const token = req.headers.authorization.split(' ')[1];
         try {
-            const decodedToken = jwt.verify(token, process.env.REACT_APP_JWT_SECRET);
-            const userId = decodedToken._id;
-            const user = await User.findById(userId);
-            if (!user) {
-                return res.status(404).json({message: 'User not found'});
-            }
+            const user=await getUserFromToken(req)
+            checkUserExists(user, res);
             const userToFollow = await User.findById(userIdToFollow);
             if (!userToFollow) {
                 return res.status(404).json({message: 'User to follow not found'});
@@ -164,7 +154,6 @@ const userControl = {
             }
             user.following.push(userIdToFollow);
             userToFollow.followers.push(userId);
-            //   await user.save();
             await Promise.all([user.save(), userToFollow.save()]);
 
             res.json({message: 'User followed successfully'});
@@ -173,28 +162,27 @@ const userControl = {
             res.status(500).json({message: 'Internal Server Error'});
         }
     },
-    unfollowUser: async (req, res) => {
-
+    followingChange: async (req, res) => {
+        const {friend, state}=req.body
+        try {
+            const user=await getUserFromToken(req)
+            checkUserExists(user, res);
+            res.json({f:friend, s:state})
+        }catch(error){
+            console.error(error);
+            res.status(500).json({message: 'Internal Server Error'});
+        }
     },
     goalSetting: async (req, res) => {
-        const token = req.headers.authorization.split(' ')[1];
-
         try {
-            const decodedToken = jwt.verify(token, process.env.REACT_APP_JWT_SECRET);
-            const userId = decodedToken._id;
-            const user = await User.findById(userId);
-            if (!user) {
-                return res.status(404).json({message: 'User not found'});
-            }
+            const user=await getUserFromToken(req)
+            checkUserExists(user, res);
             const data = req.body;
-            console.log(user, data, data.userGoal.dDay, data.userGoal.goals);
             user.goal = {
                 dDay: data.userGoal.dDay,
                 goals: data.userGoal.goals
             }
             await user.save();
-
-            // 클라이언트에게 성공 메시지 또는 필요한 응답을 보냅니다.
             res.status(200).json({message: '목표 정보가 성공적으로 저장되었습니다.'});
 
         } catch (error) {
@@ -204,19 +192,11 @@ const userControl = {
     },
     getFollowing: async (req, res) => {
         const {following} = req.body;
-        // const q=following
-        const token = req.headers.authorization.split(' ')[1];
         try {
-            const decodedToken = jwt.verify(token, process.env.REACT_APP_JWT_SECRET);
-            const userId = decodedToken._id;
-            const user = await User.findById(userId);
-            if (!user) {
-                return res.status(404).json({message: 'User not found'});
-            }
+            const user=await getUserFromToken(req)
+            checkUserExists(user, res);
             const followingUsers = await User.find({_id: {$in: following}});
-
-            const followingNames = followingUsers.map((user) => [user.name, user.email]);
-
+            const followingNames = followingUsers.map((user) => [user._id, user.name, user.email]);
             res.json({following: followingNames});
         } catch (error) {
             console.error(error);
@@ -225,19 +205,12 @@ const userControl = {
     },
     getFollowers: async (req, res) => {
         const {followers} = req.body;
-        const token = req.headers.authorization.split(' ')[1];
         try {
-            const decodedToken = jwt.verify(token, process.env.REACT_APP_JWT_SECRET);
-            const userId = decodedToken._id;
-            const user = await User.findById(userId);
-            if (!user) {
-                return res.status(404).json({message: 'User not found'});
-            }
+            const user=await getUserFromToken(req)
+            checkUserExists(user, res);
 
             const followerUsers = await User.find({_id: {$in: followers}});
-
-            const followerNames = followerUsers.map((user) => [user.name, user.email]);
-
+            const followerNames = followerUsers.map((user) => [user._id, user.name, user.email]);
             res.json({followers: followerNames});
         } catch (error) {
             console.error(error);
@@ -253,58 +226,15 @@ const userControl = {
         }
 
     },
-    updateAge: async (req, res) => {
-        const { age } = req.body;
-        const ageUpdated= await updateProps(req, res, { age }, 'age');
-        if (ageUpdated) {
-            return res.json({ state: true });
-        }
-    },
-    updateArea: async (req, res) => {
-        const {area}=req.body;
-        const areaUpdated=await updateProps(req, res, {area}, 'area');
-        if (areaUpdated) {
-            return res.json({ state: true });
-        }
-    },
-    updateWeight: async (req, res) => {
-        const {weight}=req.body;
-        const weightUpdated=await updateProps(req, res, {weight}, 'weight');
-        if (weightUpdated) {
-            return res.json({ state: true });
-        }
-    },
-    updateHeight: async (req, res) => {
-        const {height}=req.body;
-        const heightUpdated=await updateProps(req, res, {height}, 'height');
-        if (heightUpdated) {
-            return res.json({ state: true });
-        }
-    },
-    updateExercise: async (req, res) => {
-        const {exercise}=req.body;
-        const exerciseUpdated=await updateProps(req, res, {exercise}, 'exercise');
-        if (exerciseUpdated) {
-            return res.json({ state: true });
-        }
-    },
-    updateWishList: async (req, res) => {
-        const {wishList}=req.body;
-        const wishListUpdated=await updateProps(req, res, {wishList}, 'wishList');
-        if (wishListUpdated) {
-            return res.json({ state: true });
-        }
+    updateInformation: async (req, res) => {
+        const {item}=req.body;
+        await updateProfile(req,res,item)
     },
     isPasswordCorrect: async (req, res) => {
         const { password } = req.body;
-        const token = req.headers.authorization.split(' ')[1];
         try {
-            const decodedToken = jwt.verify(token, process.env.REACT_APP_JWT_SECRET);
-            const userId = decodedToken._id;
-            const user = await User.findById(userId);
-            if (!user) {
-                return res.status(404).json({message: 'User not found'});
-            }
+            const user=await getUserFromToken(req)
+            checkUserExists(user, res);
             const isPasswordCorrect = await bcrypt.compare(password, user.password);
              res.json({state:isPasswordCorrect});
         } catch (error) {
