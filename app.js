0c2816c45dc545b9c580f63d7bd951b4c0b9be67
park <park>
2023-08-30 16:15:14 +0900

    메이트 팀 회원정보, 게시판 글 등록 함수 구현

--- app.js
+++ app.js
@@ -1,26 +1,28 @@
 //패키지 임포트
 const createError = require('http-errors');
 const express = require('express');
+const app = express();
+const debug = require('debug')('server:server');
+const http = require('http');
+const {Server} = require("socket.io")
+
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-const cors=require('cors')
+const cors = require('cors')
+
+const mongoose = require('mongoose')
 require('dotenv').config()
 
 
-
-
-
-const app = express();
-const mongoose = require('mongoose')
-
 //CORS 옵션
-let corsOptions={
-  origin:[
-    'http://localhost:3000',
-    'https://pose2team.vercel.app'
-  ],
-  credentials:true
+let corsOptions = {
+    origin: [
+        'http://localhost:3000',
+        'https://pose2team.vercel.app'
+    ],
+    methods: ["GET", "POST"],
+    credentials: true
 }
 
 // view engine setup
@@ -29,7 +31,7 @@ app.set('view engine', 'jade');
 app.use(cors(corsOptions))
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -41,14 +43,14 @@ mongoose.set('strictQuery', false)
 const db = mongoose.connection
 //d
 db.on('connected', function () {
-  console.log('server connected')
+    console.log('server connected')
 })
 db.on('error', console.error)
 db.on('open', function () {
-  console.log('connected to mongo server ' + dbUrl)
+    console.log('connected to mongo server ' + dbUrl)
 })
 db.on('disconnect', function () {
-  console.log('disconnected from mongo server' + dbUrl)
+    console.log('disconnected from mongo server' + dbUrl)
 })
 
 
@@ -63,25 +65,123 @@ const usersRouter = require('./routes/users');
 const teamRouter = require('./routes/teams');
 
 //라우터 설정
-app.use('/index', indexRouter);
-app.use('/user', usersRouter);
-app.use('/team', teamRouter);
+app.use('/api/index', indexRouter);
+app.use('/api/user', usersRouter);
+app.use('/api/team', teamRouter);
+
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(function (req, res, next) {
+    next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+});
+
+const port = normalizePort(process.env.PORT || '3001');
+app.set('port', port);
+// http.listen(port, () => console.log(`Listening on port ${port}`));
+const server = http.createServer(app);
+server.listen(port, () => console.log(`server on ${port}`));
+server.on('error', onError);
+server.on('listening', onListening);
+server.on('listening', function () {
+        console.log('Express server listening on port ' + port);
+    }
+)
+
+const io = new Server(server
+    , {
+        path: '/chat',
+        cors: {
+            origin: [
+                'http://localhost:3000',
+                'https://pose2team.vercel.app']
+        },
+    }
+);
+io.on('connection', (socket) => {
+    console.log(`user connected ${socket.id}`);
+
+    // 클라이언트로부터의 메시지 수신 및 브로드캐스팅
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg); // 모든 클라이언트에게 메시지 전송
+        console.log(msg)
+    });
+
+    // 연결 해제 시 처리
+    socket.on('disconnect', () => {
+        console.log('A user disconnected from the chat');
+    });
 });
 
 
-module.exports = app;
+/**
+ * Normalize a port into a number, string, or false.
+ */
+
+function normalizePort(val) {
+    const port = parseInt(val, 10);
+
+    if (isNaN(port)) {
+        // named pipe
+        return val;
+    }
+
+    if (port >= 0) {
+        // port number
+        return port;
+    }
+
+    return false;
+}
+
+/**
+ * Event listener for HTTP server "error" event.
+ */
+
+function onError(error) {
+    if (error.syscall !== 'listen') {
+        throw error;
+    }
+
+    const bind = typeof port === 'string'
+        ? 'Pipe ' + port
+        : 'Port ' + port;
+
+    // handle specific listen errors with friendly messages
+    switch (error.code) {
+        case 'EACCES':
+            console.error(bind + ' requires elevated privileges');
+            process.exit(1);
+            break;
+        case 'EADDRINUSE':
+            console.error(bind + ' is already in use');
+            process.exit(1);
+            break;
+        default:
+            throw error;
+    }
+}
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+
+function onListening() {
+    const addr = server.address();
+    const bind = typeof addr === 'string'
+        ? 'pipe ' + addr
+        : 'port ' + addr.port;
+    debug('Listening on ' + bind);
+}
+
+// module.exports = app;
